917602e0773eff7497574159c4b9101b666e5621
Bogdan Ivanov <ivanovbogdan1994@gmail.com>
2020-11-17 11:15:16 +0200

    test commit

--- src/main.ts
+++ src/main.ts
@@ -12,7 +12,7 @@ async function bootstrap() {
         .setVersion('1.0')
         .setBasePath('api')
         .addBearerAuth()
-        .build();
+        .build(); 
     const document = SwaggerModule.createDocument(app, options);
     SwaggerModule.setup('/docs', app, document);
     await app.listen(3000);
